e318be6f10d41969a7e607dc40e1f2f77f736aa9
deviloperj <public.park.ji@gmail.com>
2023-09-19 17:57:01 +0900

    replace to the text

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
 console.log("This is a popup!");
+
+const handleClick = () => {
+  alert("hhi");
+};
--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,11 @@
-const ads = document.querySelectorAll("[id^=ad_]");
+const IMG_URL = "images/icon-16.png";
+// 일단은 광고일듯한걸 찾는다.
+const ads = document.querySelectorAll("[id^=ad_], [id^=adngin]");
 if (ads) {
   for (let i = 0; i < ads.length; i++) {
-    const el = ads[i];
-    console.log("el", el);
-    el.remove();
-    console.log("removed!");
+    const node = ads[i];
+    node.id = "asdf";
+    node.innerHTML = "✨띠용✨";
+    console.log(`${node} changed!`);
   }
 }
