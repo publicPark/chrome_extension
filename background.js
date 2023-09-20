1c55bbe170ffddf1fb2eb706845ec7e9156c7d7e
deviloperj <public.park.ji@gmail.com>
2023-09-20 15:46:21 +0900

    feat : 팝업 만들기

--- background.js
+++ background.js
@@ -1,6 +1,21 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"],
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: "OFF",
   });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const nextState = prevState === "ON" ? "OFF" : "ON";
+
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  if (nextState === "ON") {
+    console.log("on");
+  } else if (nextState === "OFF") {
+    console.log("off");
+  }
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
-console.log("This is a popup!");
+//
+const DARK_MODE = "dark-mode";
 
-const handleClick = () => {
-  alert("hhi");
-};
+const initialMode = localStorage.getItem("mode");
+if (initialMode === DARK_MODE) document.body.classList.add(DARK_MODE);
+
+const button = document.querySelector("#mode-btn");
+button.addEventListener("click", async () => {
+  console.log("test", document.body.classList);
+  document.body.classList.toggle(DARK_MODE);
+  const isDarkMode = document.body.classList.contains(DARK_MODE);
+  if (isDarkMode) localStorage.setItem("mode", DARK_MODE);
+  else localStorage.removeItem();
+});
